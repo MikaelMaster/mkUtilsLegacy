be9686a6a15eba1cffa9ca408ed40beb24299348
Mikael <mikaelpeixoto123t@gmail.com>
2022-11-27 13:57:46 -0300

    Vault.kt updates
    Took 7 minutes

--- src/main/kotlin/com/mikael/mkutilslegacy/spigot/api/lib/hooks/Vault.kt
+++ src/main/kotlin/com/mikael/mkutilslegacy/spigot/api/lib/hooks/Vault.kt
@@ -9,51 +9,49 @@ object Vault {
     private var isHooked = false
     private lateinit var economy: Economy
 
-    fun onEnable() {
+    internal fun onEnable() {
         isHooked = Bukkit.getPluginManager().isPluginEnabled("Vault")
-
         if (isHooked) {
             UtilsMain.instance.log("§e[VaultAPI] §aVault encontrado! Iniciando VaultAPI...")
-
             economy = Bukkit.getServicesManager().getRegistration(Economy::class.java).provider
-
         } else {
             UtilsMain.instance.log("§e[VaultAPI] §cVault não encontrado! VaultAPI desativado.")
         }
     }
 
-    fun onDisable() {
-        if(isHooked) {
+    internal fun onDisable() {
+        if (isHooked) {
             UtilsMain.instance.log("§e[VaultAPI] §cVaultAPI desativado.")
         }
-
         isHooked = false
     }
 
-    fun isHooked() = isHooked
+    /**
+     * API Starts.
+     */
+
+    fun isHooked(): Boolean {
+        return isHooked
+    }
 
     fun getPlayerBalance(player: Player): Double {
         if (!isHooked) error("VaultAPI isn't hooked!")
-
         return economy.getBalance(player)
     }
 
     fun setPlayerBalance(player: Player, amount: Double) {
         if (!isHooked) error("VaultAPI isn't hooked!")
-
         economy.withdrawPlayer(player, getPlayerBalance(player))
         economy.depositPlayer(player, amount)
     }
 
     fun addPlayerBalance(player: Player, amount: Double) {
         if (!isHooked) error("VaultAPI isn't hooked!")
-
         economy.depositPlayer(player, amount)
     }
 
     fun removePlayerBalance(player: Player, amount: Double) {
         if (!isHooked) error("VaultAPI isn't hooked!")
-
         economy.withdrawPlayer(player, amount)
     }
 }
